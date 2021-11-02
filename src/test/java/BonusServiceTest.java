7ace087198fdd83a6ca7dcb6e1e1495de5accac0
aa_maslov <smart.maslov@gmail.com>
2021-11-02 05:46:15 +0300

    добавлен тест

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,9 +7,8 @@ class BonusServiceTest {
         BonusService service = new BonusService();
 
         long amount = 1000_60;
-        boolean registered = true;
         long expected = 30;
-        long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, true);
         assertEquals(expected, actual);
     }
 
@@ -19,16 +18,26 @@ class BonusServiceTest {
         long amount = 1_000_000_60;
         boolean registered = true;
         long expected = 500;
-        long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, true);
         assertEquals(expected, actual);
     }
     @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegistred() {
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
-        long amount = 100000;
+        long amount = 10000;
         boolean registered = false;
-        long expected = 10;
-        long actual = service.calculate(amount,registered);
+        long expected = 1;
+        long actual = service.calculate(amount, false);
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 100_000_000;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, false);
         assertEquals(expected,actual);
     }
 }
